212b6fa372fd8516bac3d1e81b5e6a9a9119a06f
Chi Zama <cz2ej@virginia.edu>
2019-05-23 10:00:51 -0400

    Updated week1 Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from "react";
 import axios from "axios";
 import "./App.css";
+import { Map, CircleMarker, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 export default class App extends React.Component {
-  state = { name: [], id: [] };
-
+  constructor(props) {
+    super(props);
+    this.state = { info: [] };
+  }
   componentDidMount() {
     axios
       .get(
-        "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Charlottesville&opennow&key=" +
-          API_KEY
+        "https://cors-anywhere-hclaunch.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Charlottesville&opennow&key=" +
+        API_KEY
       )
       .then(res => {
-        var theName = res.data.results.map(name => name.name);
-        console.log(theName);
-        var ids = res.data.results.map(id => id.id);
+
+        const info = res.data.results;
+
+
         this.setState({
-          name: theName,
-          id: ids
+          info: info
         });
         console.log(this.state);
       });
@@ -27,18 +31,55 @@ export default class App extends React.Component {
     console.log(this.state.name);
     return (
       <div>
-        <li>
+        <Map
+          style={{ height: "480px", width: "100%" }}
+          zoom={1}
+          center={[-0.09, 51.505]}>
+          <TileLayer url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <CircleMarker
+            center={[51.505, -0.09]} />
+
+        </Map>
+        <footer>
           <ul>
-            {this.state.name.map(maps => (
-              <li>
-                {" "}
-                {maps} is in Charlottesville, VA and currently open for
-                business!
-              </li>
-            ))}
+            {this.state.info.map(mapper =>
+              (<li className="Restaurants">{mapper.name}<ul> Location: {mapper.formatted_address}<li>{mapper.price_level}</li></ul></li>)
+            )}
+
+
+
+
+
+
+
+
+
+
+
+
           </ul>
-        </li>
+        </footer>
+
+
+
+
+
+
       </div>
+
     );
   }
 }
+
+/*<div>
+  <li>
+    <ul>
+      {this.state.name.map(maps => (
+        <li>
+          {maps} is in Charlottesville, VA and currently open for
+          business!
+        </li>
+      ))}
+    </ul>
+  </li>
+</div>*/
